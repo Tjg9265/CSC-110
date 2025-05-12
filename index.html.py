7a794c41ebf1bea56d544c8e2072b3c44ddef536
Tjg9265 <Tjg9265@yahoo.com>
2025-05-12 17:09:09 -0600

    Update index.html.py

--- index.html.py
+++ index.html.py
@@ -2,23 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name= "viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thomas Garcia | WDD 130</title>
 </head>
 <body>
-    </header>
+    <header>
         <nav>
             <a href="#">Home</a>
             <a href="wwr/">Rafting Website</a>
         </nav>
     </header>
+    
     <main>
         <h1>Thomas Garcia | WDD 130</h1>  
-        <img src="c:\Users\yareo\Downloads\image1.jpeg" alt="hello, my name is Thomas Garcia" width="200">
-        <p>Hello! My name is Thomas Garcia and I am From Kansas City, USA. I enjoy reading and chilling at home with my family.</p>
+        <img src="images/image1.jpeg" alt="Hello, my name is Thomas Garcia" width="200">
+        <p>Hello! My name is Thomas Garcia and I am from Kansas City, USA. I enjoy reading and chilling at home with my family.</p>
     </main>
+    
     <footer>
-        <p> © 2025 Thomas Garcia Utah, United States of America </p>
-    <footer>
+        <p>© 2025 Thomas Garcia — Utah, United States of America</p>
+    </footer>
 </body>
 </html>
